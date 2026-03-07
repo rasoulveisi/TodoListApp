abe97ba064e1ed7ba87c09f0c9e89ee08cb25a6f
Rasoul Veisi <rasoulweysi@gmail.com>
2026-03-07 11:56:08 +0330

    Add abstractions for Category, TodoItem, and TodoList; implement new entities for TodoItem and RecurrencePattern, and update Category and TodoList entities to reflect new properties and relationships.

--- src/TodoList.Domain/Entities/TaskCategory.cs
+++
@@ -1,9 +0,0 @@
-namespace TodoList.Domain.Entities;
-
-public class TaskCategory
-{
-    public int TodoTaskId { get; set; }
-    public TodoTask? TodoTask { get; set; }
-    public int CategoryId { get; set; }
-    public Category? Category { get; set; }
-}
